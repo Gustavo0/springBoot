67bdac358b51feb58f0ea65b7c29d6b089ac8439
Gustavo Gomes <gustavo.sgomess@gmail.com>
2019-09-15 20:11:53 -0300

    COnfiguracao Spring Security + geracao token com JWT

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$I0BI..78Q24ei3xS/zBjAO7oIdd9BxYOPPScyu8uEL8xfAZnYw5aa');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
